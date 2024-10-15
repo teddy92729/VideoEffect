616ac55bc7ce243871c4498127a8deef1297dbe8
HYTeddy <teddy92729@gmail.com>
2024-10-15 10:22:31 +0800

    optmize video observer

--- src/main.mjs
+++ src/main.mjs
@@ -1,21 +1,27 @@
 import { Vapp } from "./vapp.mjs";
 import "pixi.js/unsafe-eval";
+import { log } from "./utils.mjs";
 
 let cache = new WeakMap();
 
-Array.from(document.querySelectorAll("video")).forEach((videoElement) => {
-    cache.set(videoElement, new Vapp(videoElement));
-});
-
-let observer = new MutationObserver((mutationsList, observer) => {
-    for (let mutation of mutationsList) {
-        if (mutation.type === "childList") {
-            for (let node of mutation.addedNodes) {
-                if (node.tagName === "VIDEO") {
-                    cache.set(node, new Vapp(node));
-                }
+let observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+        for (let node of mutation.addedNodes) {
+            if (node instanceof HTMLVideoElement) {
+                log("New video element detected", node);
+                cache.set(node, new Vapp(node));
             }
         }
     }
 });
-observer.observe(document.body, { childList: true, subtree: true });
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
+
+document.querySelectorAll("video").forEach((videoElement) => {
+    log("Video element detected", videoElement);
+    cache.set(videoElement, new Vapp(videoElement));
+});
+
+log("Global initialized");
