7adaef4cb1e8075092160894e37f30308c7abacb
HYTeddy <teddy92729@gmail.com>
2024-10-20 05:39:19 +0800

    format

--- src/shaders/vertex3.glsl
+++ src/shaders/vertex3.glsl
@@ -6,23 +6,20 @@ uniform vec4 uInputSize;
 uniform vec4 uOutputFrame;
 uniform vec4 uOutputTexture;
 
-vec4 filterVertexPosition(void)
-{
-    vec2 position=aPosition*uOutputFrame.zw+uOutputFrame.xy;
-    
-    position.x=position.x*(2./uOutputTexture.x)-1.;
-    position.y=position.y*(2.*uOutputTexture.z/uOutputTexture.y)-uOutputTexture.z;
-    
-    return vec4(position,0.,1.);
+vec4 filterVertexPosition(void) {
+    vec2 position = aPosition * uOutputFrame.zw + uOutputFrame.xy;
+
+    position.x = position.x * (2.f / uOutputTexture.x) - 1.f;
+    position.y = position.y * (2.f * uOutputTexture.z / uOutputTexture.y) - uOutputTexture.z;
+
+    return vec4(position, 0.f, 1.f);
 }
 
-vec2 filterTextureCoord(void)
-{
-    return aPosition*(uOutputFrame.zw*uInputSize.zw);
+vec2 filterTextureCoord(void) {
+    return aPosition * (uOutputFrame.zw * uInputSize.zw);
 }
 
-void main(void)
-{
-    gl_Position=filterVertexPosition();
-    vTextureCoord=filterTextureCoord();
+void main(void) {
+    gl_Position = filterVertexPosition();
+    vTextureCoord = filterTextureCoord();
 }
